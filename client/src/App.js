1f8987d0e93727ae084853c55172891a28fef8cf
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-07-17 10:55:07 -0400

    move folders

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import { useLoadScript } from "@react-google-maps/api"
+
+import Map from "./components/Map";
+
+import './App.css';
+
+function App() {
+
+  const googleAPIkey = process.env.REACT_APP_PUBLIC_GOOGLE_MAPS_API_KEY
+
+  const { isLoaded } =  useLoadScript({
+    googleMapsApiKey: googleAPIkey,
+    libraries: ["places"]
+  })
+
+  if (!isLoaded) return <div>Loading...</div>
+  return (
+    <div >
+      <Map />
+    </div>
+  );
+}
+
+export default App;
